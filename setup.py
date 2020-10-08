cbed09ea08fece4f4f61880e906aabdd5cb22d18
Steve Nyemba <steve@the-phi.com>
2020-10-08 17:15:14 -0500

    version 1.2.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read() 
 args    = {
     "name":"data-transport",
-    "version":"1.2.2",
+    "version":"1.2.4",
     "author":"The Phi Technology LLC","author_email":"info@the-phi.com",
     "license":"MIT",
     "packages":["transport"]}
