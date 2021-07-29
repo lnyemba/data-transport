10adde7a083d6c04ec860ceb58bdc77cc1bc27b5
Steve Nyemba <steve@the-phi.com>
2021-07-28 22:32:07 -0500

    bug fix: sqlwriter

--- transport/sql.py
+++ transport/sql.py
@@ -58,6 +58,7 @@ class SQLRW :
         # _handler = SQLWriter.DRIVERS[_args['provider']]
         _handler = SQLWriter.REFERENCE[_provider]['handler']
         self._dtype = SQLWriter.REFERENCE[_provider]['dtype'] if 'dtype' not in _args else _args['dtype']
+        self._provider = _provider
         if _handler == nz :
             _info['database'] = _info['dbname']
             _info['securityLevel'] = 0
@@ -199,11 +200,13 @@ class SQLWriter(SQLRW,Writer):
                 # _sql = _sql.replace(":fields",_fields)
                 # _sql = _sql.replace(":values",",".join(["%("+name+")s" for name in self.fields]))
                 _sql = _sql.replace("(:fields)","")
-                values = ", ".join('?'*len(self.fields))
+                values = ", ".join("?"*len(self.fields)) if self._provider == 'netezza' else ",".join(["%s" for name in self.fields])
                 _sql = _sql.replace(":values",values)
+                print (_sql)
                 
                 # for row in info :
                 #     values = ["'".join(["",value,""]) if not str(value).isnumeric() else value for value in row.values()]
+                
                 cursor.executemany(_sql,info)   
             
             # self.conn.commit()
